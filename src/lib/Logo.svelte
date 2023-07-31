5097040c58d9a89feda3b1244a8edf5f27dba44e
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-08-01 11:34:19 +1200

    Trying create a airplane page

---
+++ src/lib/Logo.svelte
@@ -0,0 +1,5 @@
+<header style="background-color:black;">
+    <img src="AirplaneLogo.jfif" alt="Airline Logo" width="100" height="100">
+</header>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Hello, welcome to my Svelte site</h1>
+<script>
+    import Logo from '$lib/Logo.svelte';
+</script>
+
+<Logo />
+
+<body>
+    <h2>Type Of Airplane</h2>
+        <ul>
+            <li><a href="https://skybrary.aero/articles/propeller#:~:text=Description,applications%2C%20by%20an%20electric%20motor.">Propeller Airplane</a></li>
+            <li><a href="https://nbaa.org/business-aviation/business-aircraft/jet-aircraft/">Jet Airplane</a></li>
+            <li><a href="https://en.wikipedia.org/wiki/Fighter_aircraft">Fighter Jet</a></li>
+        </ul>
+</body>
+
+
 
