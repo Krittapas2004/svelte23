d05b62573072dfb9b657d998b3393aa651e5c8d2
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-08-04 09:47:56 +1200

    Html and Css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+
+    <link rel="stylesheet" href="static/mainIndex.css">
+
+</svelte:head>
+
+<slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,14 @@
 <body>
     <h2>Type Of Airplane</h2>
         <ul>
-            <li><a href="https://skybrary.aero/articles/propeller#:~:text=Description,applications%2C%20by%20an%20electric%20motor.">Propeller Airplane</a></li>
-            <li><a href="https://nbaa.org/business-aviation/business-aircraft/jet-aircraft/">Jet Airplane</a></li>
-            <li><a href="https://en.wikipedia.org/wiki/Fighter_aircraft">Fighter Jet</a></li>
+            <li>Jet Airplane</li>
+            <img src ="https://media.cnn.com/api/v1/images/stellar/prod/220523085523-01-worlds-fastest-passenger-jet.jpg?c=4x3" alt="Jet Airplane" style="width:500px;height:400px;">
+            <li>Propeller Airplane</li>
+            <li>Fighter Jet</li>
         </ul>
+    
 </body>
 
+<slot/>
 
 
