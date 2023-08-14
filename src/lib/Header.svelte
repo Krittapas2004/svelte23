ac9cc9cf0b5c612baebece1995b6868bbaab6e9f
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-08-15 11:45:05 +1200

    Finished the main cat picture and positioning the logo

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,6 @@
 
 <nav>
-    <img id="Logo" src="catlogo.png" alt="Cat Logo">
+    <img id="logo" src="img/catlogo.png" alt="Cat Logo">
     <ul>
         <li><a href="routes/page.svelte">Home</a></li>
         <li><a href="Information/page.svelte">Infomation</a></li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 
 <div class="mainIndexContainer">
-    <img id="CatLogo" src="catimg1.png" alt="Cat logo" width="1000">
     <div class="mainIndexBox">
         
     </div>
 </div>
 
-<div class="mainIndexText">
+<div class="titleIndex">
+    <body>
+        <h1>I am cat</h1>
+    </body>
 </div>
 <slot/>
 
