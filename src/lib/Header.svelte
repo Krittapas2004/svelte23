50d0878f3917bdcbdc60c7017b129e0334f36a68
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-08-22 11:54:29 +1200

    Finished creating search bar in header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -5,5 +5,12 @@
         <li><a href="CatType/page.svelte">Cat Type</a></li>
         <li><a href="Location/page.svelte">Location</a></li>
         <li><a href="Store/page.svelte">Store</a></li>
+        
     </ul>
+    <div class="container">
+        <form action="https://www.google.com/" class="SearchBar">
+            <input type="text" placeholder="Search">
+            <button type="submit"><img src="img/searchBar.png"></button>
+        </form>
+    </div>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,11 @@
 
 <div class="mainIndexContainer">
-    <div class="mainIndexBox">
-        
-    </div>
+    
 </div>
 
 <div class="titleIndex">
     <body>
-        <div class="container">
-            <form action="https://www.google.com/" class="SearchBar">
-                <input type="text" placeholder="Search" name="q">
-                <button type="submit"><img src="searchIcon.png"></button>
-            </form>
-        </div>
+        
     </body>
 </div>
 <slot/>
