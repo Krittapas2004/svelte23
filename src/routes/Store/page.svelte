57967fbec90f6d54a8fa69f9f0d5926cb753d30e
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-08-25 11:43:57 +1200

    Finished creating a Login button

--- src/routes/Store/page.svelte
+++ src/routes/Store/page.svelte
@@ -1,9 +1,21 @@
-<nav style="background-color:white;">
-    <img id="Logo" src="catlogo.png" alt="Cat Logo">
+<nav>
+    <img id="logo" src="img/catlogo.png" alt="Cat Logo">
     <ul>
-        <li><a href="routes/page.svelte">Home</a></li>
-        <li><a href="CatType/page.svelte">Cat Type</a></li>
-        <li><a href="Location/page.svelte">Location</a></li>
-        <li><a href="Store/page.svelte">Store</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="CatType">Cat Type</a></li>
+        <li><a href="Location">Location</a></li>
+        <li><a href="Store">Store</a></li>
+        
     </ul>
+    <div class="container">
+        <form action="https://www.google.com/" class="SearchBar">
+            <input type="text" placeholder="Search">
+            <button type="submit"><img src="img/searchBar.png"></button>
+        </form>
+    </div>
+    <div id="login_bar" style="text-align: right">                                               
+        <ul id="login_signup">
+            <li><a href="Login" id="login_link">Login <span>&#x25c0;</span></a></li>
+        </ul>
+</div>
 </nav>
