cbe1bedbdc6b51e48ae2181d135a80b4a465326c
Ummmm <kittapaschinthumrucks@MacBook-Pro.local>
2023-10-28 23:06:07 +1300

    Adding a layout for about us in the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,34 @@
+<div class="background">
+    <div class="video">
+        <video autoplay loop muted>
+            <source src="video/catVid.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
+    </div>
 
-<video width="100%" height="900" autoplay loop muted>
-    <source src="video/background.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-</video>
-
-<div class="sliding-text">
-    <span>About</span>
-    <span>Us</span>
+    <div class="sliding-text">
+        <span>About</span>
+        <span>Us</span>
+    </div>
+    <div class="container1">
+        <div class="insideContainer1">
+            <h2>About Our Purrpose: Getting to Know Us</h2>
+            <p>
+                At our website, we are passionate about all things feline. Our
+                love for cats drives our mission to provide you with valuable
+                information, resources, and a sense of community for cat
+                enthusiasts worldwide. We're a dedicated team of cat lovers,
+                committed to sharing our expertise in cat care, health,
+                behavior, and more. With a paw-sitive approach, we aim to assist
+                cat owners in giving their furry friends the best possible
+                lives. Join us in celebrating the wonderful world of cats!
+                <button class="aboutUs-button">Learn more</button>
+            </p>
+        </div>
+        <div class="aboutUs-img">
+            <img src="img/aboutUs.png" alt="About Us Cat" />
+        </div>
+    </div>
 </div>
 
 <slot />
