06b191d38adc88f0cc7df73c1bc0285ddbb4f37e
Ummmm <kittapaschinthumrucks@MacBook-Pro.local>
2023-10-30 20:08:27 +1300

    Make a layout for cat type page

---
+++ src/routes/catType/+page.svelte
@@ -0,0 +1,13 @@
+<div class="Our-Cat">
+    <h1>Our Cat</h1>
+</div>
+
+<div class="catCardContainer">
+    <div class="slide-catCardContainer">
+        <div class="card-wrapper">
+            <div class="card">
+                
+            </div>
+        </div>
+    </div>
+</div>
