c5593e2665cee289d271fca0f1a7f9ae7a572279
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-10-31 11:55:38 +1300

    Adding the my Key to the link

--- src/routes/catType/+page.svelte
+++ src/routes/catType/+page.svelte
@@ -1,15 +1,27 @@
 <script>
-    
+  import { onMount } from "svelte";
+
+  let imgs = [];
+  const BASE_URL = `https://api.unsplash.com`;
+  onMount(() => {
+    fetch(
+      `${BASE_URL}/search/photos?query=cats&client_id=J8xlJ50rIrCU5MARTJ_59uX-eW75wA5TfHSnJf0ZfVU`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        imgs = data.results;
+      });
+  });
 </script>
 
 <div class="Our-Cat">
-    <h1>Our Cat</h1>
+  <h1>Our Cat</h1>
 </div>
 
 <div class="catCardContainer">
-    <div class="slide-catCardContainer">
-        <div class="card-wrapper">
-            <div class="card" />
-        </div>
+  <div class="slide-catCardContainer">
+    <div class="card-wrapper">
+      <div class="card" />
     </div>
+  </div>
 </div>
