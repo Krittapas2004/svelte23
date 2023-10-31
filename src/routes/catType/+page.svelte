d1678ad2b58d359e687f4518abd6dc45277c378a
Ummmm <kittapaschinthumrucks@MacBook-Pro.local>
2023-10-31 23:14:22 +1300

    Adding a css style for the image

--- src/routes/catType/+page.svelte
+++ src/routes/catType/+page.svelte
@@ -1,39 +1,41 @@
-
 <script>
   import { onMount } from "svelte";
 
-let images = [];
-const BASE_URL = "https://api.unsplash.com/search/photos";
-const API_KEY = "J8xlJ50rIrCU5MARTJ_59uX-eW75wA5TfHSnJf0ZfVU";
-const query = "cats";
+  let images = [];
+  const BASE_URL = "https://api.unsplash.com/search/photos";
+  const API_KEY = "J8xlJ50rIrCU5MARTJ_59uX-eW75wA5TfHSnJf0ZfVU";
+  const query = "cats";
 
-onMount(async () => {
-  try {
-    const response = await fetch(`${BASE_URL}?query=${query}&client_id=${API_KEY}`);
-    if (response.ok) {
-      const data = await response.json();
-      images = data.results;
-    } else {
-      console.error("Error fetching data:", response.statusText);
+  onMount(async () => {
+    try {
+      const response = await fetch(
+        `${BASE_URL}?query=${query}&client_id=${API_KEY}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        images = data.results;
+      } else {
+        console.error("Error fetching data:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-});
+  });
 </script>
 
 <div class="Our-Cat">
-<h1>Our Cat</h1>
+  <h1>Our Cat</h1>
 </div>
-
-{#each images as image}
-<img src={image.urls.regular} alt="Cat" />
-{/each}
 
 <div class="catCardContainer">
   <div class="slide-catCardContainer">
     <div class="card-wrapper">
       <div class="card" />
+      <div class="image-box">
+        {#each images as image}
+          <img src={image.urls.regular} alt="Cat" />
+        {/each}
+      </div>
     </div>
   </div>
 </div>
