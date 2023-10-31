feb5b5eba5d797fd669e763c7081f6a7b30ab30e
Ummmm <kittapaschinthumrucks@MacBook-Pro.local>
2023-10-31 22:34:30 +1300

    Trying to get the picture from the unsplash API to show in the webpage

--- src/routes/catType/+page.svelte
+++ src/routes/catType/+page.svelte
@@ -1,18 +1,26 @@
+
 <script>
   import { onMount } from "svelte";
 
-  let imgs = [];
+  let img = [];
   const BASE_URL = `https://api.unsplash.com`;
+  const API_KEY = "J8xlJ50rIrCU5MARTJ_59uX-eW75wA5TfHSnJf0ZfVU";
+
   onMount(() => {
     fetch(
-      `${BASE_URL}/search/photos?query=cats&client_id=J8xlJ50rIrCU5MARTJ_59uX-eW75wA5TfHSnJf0ZfVU`
+      `${BASE_URL}/search/photos?query=cats&client_id=${API_KEY}`
     )
       .then((res) => res.json())
       .then((data) => {
-        imgs = data.results;
+        imgs.push(data.results)
       });
   });
+  
 </script>
+
+{#each imgs as img}
+    <img src = {img.urls.regular}/>
+{/each}
 
 <div class="Our-Cat">
   <h1>Our Cat</h1>
