85bef2ed1229c7b16b0392aea85126930f359156
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-11-03 11:25:30 +1300

    Adding a social media logo in footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,19 @@
 <footer>
-<div class=container>
-    <p>&copy; 2023 cats. All rigths reserved.</p>
-</div>
+  <div class="container">
+    <p>&copy; 2023 Krit. All rigths reserved.</p>
+  </div>
+  <div class="allSocialMedia">
+    <div class="facebook-cats">
+      <a href="https://www.facebook.com" target="_blank">
+    <i class="fa-brands fa-facebook" style="color: #005eff; font-size: 3em;"></i>
+    </div>
+    <div class="ig-cats">
+        <a href="https://www.instagram.com" target="_blank">
+        <i class="fa-brands fa-instagram fa-flip" style="color: #c71fa8; font-size: 3em;"></i>
+    </div>
+    <div class="twitter-cats">
+        <a href="https://twitter.com" target="_blank">
+        <i class="fa-brands fa-x-twitter fa-flip-both" style="color: #000000; font-size: 3em;"></i>
+    </div>
+  </div>
 </footer>
