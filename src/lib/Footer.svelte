c882265771f31443aac41e7731b21b2fcc664382
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-11-03 11:27:55 +1300

    Changing the color of instagram logo to be fade blue, pink, yellow color

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -5,15 +5,31 @@
   <div class="allSocialMedia">
     <div class="facebook-cats">
       <a href="https://www.facebook.com" target="_blank">
-    <i class="fa-brands fa-facebook" style="color: #005eff; font-size: 3em;"></i>
+        <i
+          class="fa-brands fa-facebook"
+          style="color: #005eff; font-size: 3em;"
+        />
+      </a>
     </div>
     <div class="ig-cats">
-        <a href="https://www.instagram.com" target="_blank">
-        <i class="fa-brands fa-instagram fa-flip" style="color: #c71fa8; font-size: 3em;"></i>
+      <a href="https://www.instagram.com" target="_blank">
+        <i
+          class="fa-brands fa-instagram fa-flip"
+          style="font-size: 3em;
+            background: linear-gradient(90deg, #0000FF, #FF69B4, #FFFF00);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            color: transparent;"
+        />
+      </a>
     </div>
     <div class="twitter-cats">
-        <a href="https://twitter.com" target="_blank">
-        <i class="fa-brands fa-x-twitter fa-flip-both" style="color: #000000; font-size: 3em;"></i>
+      <a href="https://twitter.com" target="_blank">
+        <i
+          class="fa-brands fa-x-twitter fa-flip-both"
+          style="color: #000000; font-size: 3em;"
+        />
+      </a>
     </div>
   </div>
 </footer>
