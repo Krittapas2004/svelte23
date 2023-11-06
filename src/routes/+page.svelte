533d36da8e838370952c53477b0e87dfcd50cceb
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-11-06 16:53:31 +1300

    Adding a video again but using the ifrane from youtube

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,40 @@
 <div class="background">
-    <div class="video">
-        <video autoplay loop muted>
-            <source src=".mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-        </video>
-    </div>
+  <div class="video-container">
+    <iframe
+      class="video-iframe"
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/iKAewWqera8?si=zJIWPvZBUAc5QXW9"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
+  </div>
 
-    <div class="sliding-text">
-        <span>About</span>
-        <span>Us</span>
+  <div class="sliding-text">
+    <span>About</span>
+    <span>Us</span>
+  </div>
+  <div class="container1">
+    <div class="insideContainer1">
+      <h2>About Our Purrpose: Getting to Know Us</h2>
+      <p>
+        At our website, we are passionate about all things feline. Our love for
+        cats drives our mission to provide you with valuable information,
+        resources, and a sense of community for cat enthusiasts worldwide. We're
+        a dedicated team of cat lovers, committed to sharing our expertise in
+        cat care, health, behavior, and more. With a paw-sitive approach, we aim
+        to assist cat owners in giving their furry friends the best possible
+        lives. Join us in celebrating the wonderful world of cats!
+        <br />
+        <button class="aboutUs-button">Learn more</button>
+      </p>
     </div>
-    <div class="container1">
-        <div class="insideContainer1">
-            <h2>About Our Purrpose: Getting to Know Us</h2>
-            <p>
-                At our website, we are passionate about all things feline. Our
-                love for cats drives our mission to provide you with valuable
-                information, resources, and a sense of community for cat
-                enthusiasts worldwide. We're a dedicated team of cat lovers,
-                committed to sharing our expertise in cat care, health,
-                behavior, and more. With a paw-sitive approach, we aim to assist
-                cat owners in giving their furry friends the best possible
-                lives. Join us in celebrating the wonderful world of cats!
-                <br />
-                <button class="aboutUs-button">Learn more</button>
-            </p>
-        </div>
-        <div class="aboutUs-img">
-            <img src="img/aboutUs.png" alt="About Us Cat" />
-        </div>
+    <div class="aboutUs-img">
+      <img src="img/aboutUs.png" alt="About Us Cat" />
     </div>
+  </div>
 </div>
 
 <slot />
