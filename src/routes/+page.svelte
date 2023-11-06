defbaecaa8f196bad6c4062f900516f1bbee903d
Ummmm <kittapaschinthumrucks@MacBook-Pro.local>
2023-11-06 19:31:39 +1300

    Merge the video into main branch and use css to style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <div class="background">
-  <div class="video-container">
-    <video width="100%" height="900" autoplay loop muted>
+  <div class="background-vid">
+    <video autoplay loop muted>
       <source src="video/backgroundvid.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   </div>
-
   <div class="sliding-text">
     <span>About</span>
     <span>Us</span>
