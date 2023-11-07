54360a6f4c5648a48ff9005bf95f278acd276190
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-11-08 10:46:13 +1300

    Adding a on click to a button in porduct page

--- src/routes/store/+page.svelte
+++ src/routes/store/+page.svelte
@@ -1,67 +1,76 @@
 <section class="product">
-    <h2 class="product-category">Our Product</h2>
-    <div class="gallery">
-        <button class="previous-button"><i class="fa-solid fa-arrow-left"></i></button>    
-        <div class="product-container">
-            <div class="whole-product">
-                <div class="product-card">
-                    <div class="product-image">
-                        <img src="img/catTeaser.png" class="first-product" alt="">
-                    </div>
-                    <div class="product-info">
-                    <h2 class="product-brand">Cat Teaser</h2>
-                    <p class="product-description">Feather teaser wand that cats love to chase</p>
-                    <span class="price">$20</span>
-                    </div>
-                </div>
-
-                <div class="product-card">
-                    <div class="product-image">
-                        <img src="img/catTeaser.png" class="first-product" alt="">
-                    </div>
-                    <div class="product-info">
-                    <h2 class="product-brand">Cat Teaser</h2>
-                    <p class="product-description">Feather teaser wand that cats love to chase</p>
-                    <span class="price">$20</span>
-                    </div>
-                </div>
-
-                <div class="product-card">
-                    <div class="product-image">
-                        <img src="img/catTeaser.png" class="first-product" alt="">
-                    </div>
-                    <div class="product-info">
-                    <h2 class="product-brand">Cat Teaser</h2>
-                    <p class="product-description">Feather teaser wand that cats love to chase</p>
-                    <span class="price">$20</span>
-                    </div>
-                </div>
-
-                <div class="product-card">
-                    <div class="product-image">
-                        <img src="img/catTeaser.png" class="first-product" alt="">
-                    </div>
-                    <div class="product-info">
-                    <h2 class="product-brand">Cat Teaser</h2>
-                    <p class="product-description">Feather teaser wand that cats love to chase</p>
-                    <span class="price">$20</span>
-                    </div>
-                </div>
-
-                <div class="product-card">
-                    <div class="product-image">
-                        <img src="img/catTeaser.png" class="first-product" alt="">
-                    </div>
-                    <div class="product-info">
-                    <h2 class="product-brand">Cat Teaser</h2>
-                    <p class="product-description">Feather teaser wand that cats love to chase</p>
-                    <span class="price">$20</span>
-                    </div>
-                </div>
-            </div>
+  <h2 class="product-category">Our Product</h2>
+  <div class="gallery">
+    <button on:click={} class="previous-button"><i class="fa-solid fa-arrow-left" /></button
+    >
+    <div class="product-container">
+      <div class="whole-product">
+        <div class="product-card">
+          <div class="product-image">
+            <img src="img/catTeaser.png" class="first-product" alt="" />
+          </div>
+          <div class="product-info">
+            <h2 class="product-brand">Cat Teaser</h2>
+            <p class="product-description">
+              Feather teaser wand that cats love to chase
+            </p>
+            <span class="price">$20</span>
+          </div>
         </div>
-        <button class="next-button"><i class="fa-solid fa-arrow-left"></i></button> 
+
+        <div class="product-card">
+          <div class="product-image">
+            <img src="img/catTeaser.png" class="first-product" alt="" />
+          </div>
+          <div class="product-info">
+            <h2 class="product-brand">Cat Teaser</h2>
+            <p class="product-description">
+              Feather teaser wand that cats love to chase
+            </p>
+            <span class="price">$20</span>
+          </div>
+        </div>
+
+        <div class="product-card">
+          <div class="product-image">
+            <img src="img/catTeaser.png" class="first-product" alt="" />
+          </div>
+          <div class="product-info">
+            <h2 class="product-brand">Cat Teaser</h2>
+            <p class="product-description">
+              Feather teaser wand that cats love to chase
+            </p>
+            <span class="price">$20</span>
+          </div>
+        </div>
+
+        <div class="product-card">
+          <div class="product-image">
+            <img src="img/catTeaser.png" class="first-product" alt="" />
+          </div>
+          <div class="product-info">
+            <h2 class="product-brand">Cat Teaser</h2>
+            <p class="product-description">
+              Feather teaser wand that cats love to chase
+            </p>
+            <span class="price">$20</span>
+          </div>
+        </div>
+
+        <div class="product-card">
+          <div class="product-image">
+            <img src="img/catTeaser.png" class="first-product" alt="" />
+          </div>
+          <div class="product-info">
+            <h2 class="product-brand">Cat Teaser</h2>
+            <p class="product-description">
+              Feather teaser wand that cats love to chase
+            </p>
+            <span class="price">$20</span>
+          </div>
+        </div>
+      </div>
     </div>
+    <button on:click={} class="next-button"><i class="fa-solid fa-arrow-left" /></button>
+  </div>
 </section>
-
-
