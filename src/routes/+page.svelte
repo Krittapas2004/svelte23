44bdd4a67ba2b4a5578a64a126b786f485230e98
Ummmm <kittapaschinthumrucks@MacBook-Pro.local>
2023-11-08 16:59:46 +1300

    Adding a display flex for about us in main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,23 +9,26 @@
     <span>About</span>
     <span>Us</span>
   </div>
-  <div class="container1">
-    <div class="insideContainer1">
-      <h2>About Our Purrpose: Getting to Know Us</h2>
-      <p>
-        At our website, we are passionate about all things feline. Our love for
-        cats drives our mission to provide you with valuable information,
-        resources, and a sense of community for cat enthusiasts worldwide. We're
-        a dedicated team of cat lovers, committed to sharing our expertise in
-        cat care, health, behavior, and more. With a paw-sitive approach, we aim
-        to assist cat owners in giving their furry friends the best possible
-        lives. Join us in celebrating the wonderful world of cats!
-        <br />
-        <button class="aboutUs-button">Learn more</button>
-      </p>
-    </div>
-    <div class="aboutUs-img">
-      <img src="img/aboutUs.png" alt="About Us Cat" />
+  <div class="display-flex-img">
+    <div class="container1">
+      <div class="insideContainer1">
+        <h2>About Our Purrpose: Getting to Know Us</h2>
+        <p>
+          At our website, we are passionate about all things feline. Our love
+          for cats drives our mission to provide you with valuable information,
+          resources, and a sense of community for cat enthusiasts worldwide.
+          We're a dedicated team of cat lovers, committed to sharing our
+          expertise in cat care, health, behavior, and more. With a paw-sitive
+          approach, we aim to assist cat owners in giving their furry friends
+          the best possible lives. Join us in celebrating the wonderful world of
+          cats!
+          <br />
+          <button class="aboutUs-button">Learn more</button>
+        </p>
+      </div>
+      <div class="aboutUs-img">
+        <img src="img/aboutUs.png" alt="About Us Cat" />
+      </div>
     </div>
   </div>
 </div>
