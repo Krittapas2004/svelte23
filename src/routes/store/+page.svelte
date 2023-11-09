10accc0e41d4f18622c3f55070b423ff54bc4581
Ummmm <kittapaschinthumrucks@MacBook-Pro.local>
2023-11-09 18:11:26 +1300

    Calling a js  function in html page

--- src/routes/store/+page.svelte
+++ src/routes/store/+page.svelte
@@ -2,7 +2,7 @@
   let product1 = "/img/catTeaser.png";
   let product2 = "/img/catToy.png";
   let imgIndex = 0;
- 
+
   function scrollRight() {
     imgIndex++;
     if (imgIndex >= 2) {
@@ -19,11 +19,11 @@
 
   function img(index) {
     const imgArray = [product1, product2, product1, product2];
-  
+
     if (index >= 0 && index < imgArray.length) {
       return imgArray[index];
     } else {
-      return ""; 
+      return "";
     }
   }
 </script>
@@ -34,76 +34,27 @@
 <section class="product">
   <h2 class="product-category">Our Product</h2>
   <div class="gallery">
-    <button class="previous-button"><i class="fa-solid fa-arrow-left" /></button
-    >
+    <i on:click={scrollLeft} class="fa-solid fa-arrow-left previous-button" />
     <div class="product-container">
-      <div class="whole-product">
-        <div class="product-card">
-          <div class="product-image">
-            <img src="img/catTeaser.png" class="first-product" alt="" />
+      {#each [1, 2, 3, 4] as imgProducts, i}
+        {#if i >= imgIndex && i < imgIndex + 3}
+          <div class="whole-product">
+            <div class="product-card">
+              <div class="product-image">
+                <img src={img(i)} class="first-product" alt="" />
+              </div>
+              <div class="product-info">
+                <h2 class="product-brand">Cat Teaser</h2>
+                <p class="product-description">
+                  Feather teaser wand that cats love to chase
+                </p>
+                <span class="price">$20</span>
+              </div>
+            </div>
           </div>
-          <div class="product-info">
-            <h2 class="product-brand">Cat Teaser</h2>
-            <p class="product-description">
-              Feather teaser wand that cats love to chase
-            </p>
-            <span class="price">$20</span>
-          </div>
-        </div>
-
-        <div class="product-card">
-          <div class="product-image">
-            <img src="img/catTeaser.png" class="first-product" alt="" />
-          </div>
-          <div class="product-info">
-            <h2 class="product-brand">Cat Teaser</h2>
-            <p class="product-description">
-              Feather teaser wand that cats love to chase
-            </p>
-            <span class="price">$20</span>
-          </div>
-        </div>
-
-        <div class="product-card">
-          <div class="product-image">
-            <img src="img/catTeaser.png" class="first-product" alt="" />
-          </div>
-          <div class="product-info">
-            <h2 class="product-brand">Cat Teaser</h2>
-            <p class="product-description">
-              Feather teaser wand that cats love to chase
-            </p>
-            <span class="price">$20</span>
-          </div>
-        </div>
-
-        <div class="product-card">
-          <div class="product-image">
-            <img src="img/catTeaser.png" class="first-product" alt="" />
-          </div>
-          <div class="product-info">
-            <h2 class="product-brand">Cat Teaser</h2>
-            <p class="product-description">
-              Feather teaser wand that cats love to chase
-            </p>
-            <span class="price">$20</span>
-          </div>
-        </div>
-
-        <div class="product-card">
-          <div class="product-image">
-            <img src="img/catTeaser.png" class="first-product" alt="" />
-          </div>
-          <div class="product-info">
-            <h2 class="product-brand">Cat Teaser</h2>
-            <p class="product-description">
-              Feather teaser wand that cats love to chase
-            </p>
-            <span class="price">$20</span>
-          </div>
-        </div>
-      </div>
+        {/if}
+      {/each}
     </div>
-    <button class="next-button"><i class="fa-solid fa-arrow-left" /></button>
+    <i on:click={scrollRight} class="fa-solid fa-arrow-left next-button" />
   </div>
 </section>
